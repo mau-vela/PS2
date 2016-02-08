fcf93b6d9b0b2b9c366d0c83a039de3d3d5550d1
Mauricio Vela <mauricioandresvela@gmail.com>
2016-02-08 14:49:14 -0500

    Erase something

--- Vela_PS2.R
+++ Vela_PS2.R
@@ -10,12 +10,4 @@ thefunction <- function(x, returnm=TRUE, returnd=TRUE){
     m= max(x-log(1+(1/(1:9)), base=10))
     star=ifelse(m<0.851,"NONE",ifelse(m<0.967,"*", ifelse(m<1.212, "**",  "***")))
     m=list(m, star)
-  }
-  if (returnd==TRUE) {
-    d=sqrt(sum((x-log(1+(1/(1:9)), base=10))^2))
-    star=ifelse(d<0.851,"NONE",ifelse(d<0.967,"*", ifelse(d<1.212, "**",  "***")))
-    d=list(d, star)
-  }
-  return(list(digits=x, m=m,d=d))
-}
-thefunction(x, returnm=F)
+
