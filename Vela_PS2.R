7d404ba14c6661cc434ed3b28cec341e43067ca7
Mauricio Vela <mauricioandresvela@gmail.com>
2016-02-08 17:17:15 -0500

    Some modifications

--- Vela_PS2.R
+++ Vela_PS2.R
@@ -2,20 +2,25 @@
 #PS2
 
 
-x <- c(0,0,0,0,0,0,0,0,1)
-thefunction <- function(x, returnm=TRUE, returnd=TRUE){
-  m=NULL
-  d=NULL
+#start creating the function
+bendfordlaw <- function(x, returnm=TRUE, returnd=TRUE){
+  m <- NULL
+  d <- NULL
+  n <- length(first_digits)
   if (returnm==TRUE) {
-    m= max(x-log(1+(1/(1:9)), base=10))
+    m= max(x-log10(1+(1/(1:n))))
     star=ifelse(m<0.851,"NONE",ifelse(m<0.967,"*", ifelse(m<1.212, "**",  "***")))
     m=list(m, star)
   }
   if (returnd==TRUE) {
-    d=sqrt(sum((x-log(1+(1/(1:9)), base=10))^2))
+    d=sqrt(sum((x-log10(1+(1/(1:n))))^2))
     star=ifelse(d<0.851,"NONE",ifelse(d<0.967,"*", ifelse(d<1.212, "**",  "***")))
     d=list(d, star)
   }
   return(list(digits=x, m=m,d=d))
 }
-thefunction(x, returnm=F)
+
+
+
+x <- c(0,0,0.1,0.1,0,0,0,0,0.8)
+bendfordlaw(x, returnm=F)
